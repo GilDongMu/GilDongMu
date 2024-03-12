ece546258cad91abace299edc2bce602f2e0a777
eonpain <zmfltmwnsrl@naver.com>
2024-03-12 19:18:59 +0900

    refactor: 최신순,인기순 여행글 리스트 캐러셀에 적용될 다른 전역 css 추가

--- components/carousel/custom/index.tsx
+++ components/carousel/custom/index.tsx
@@ -25,7 +25,7 @@ function SampleNextArrow(props: { className: any; style: any; onClick: any }) {
       }}
     >
       <Image
-        className="-translate-x-10"
+        className="-translate-y-20"
         src="icons/arrow_right_primary.svg"
         alt="캐러셀다음버튼"
         width={32}
@@ -57,7 +57,7 @@ function SamplePrevArrow(props: { className: any; style: any; onClick: any }) {
       }}
     >
       <Image
-        className="-translate-x-10"
+        className="-translate-y-20"
         src="icons/arrow_left_primary.svg"
         alt="캐러셀이전버튼"
         width={32}
@@ -113,7 +113,7 @@ function Responsive() {
     ],
   };
   return (
-    <div className="slider-container">
+    <div className="slider-container my-custom-slider">
       <Slider {...settings}>
         <div className="flex w-40 h-40 border-2 border-black">
           <h3>1</h3>
